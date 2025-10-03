f0f0043ee265cd4f90378ae7be1a4051bb19cf0d
langworr <langwor@gmail.com>
2025-10-03 07:29:50 +0200

    before change block mark

--- index.mjs
+++ index.mjs
@@ -2,42 +2,19 @@
 //
 'use strict'
 
-// Returns the sidenote number as a string
-// If env.docId is set, it is included in the id to make it unique across documents
-function render_sidenote_number (tokens, idx, options, env/*, slf */) {
-  // const n = Number(tokens[idx].meta.id + 101).toString()
-  const n = Number(101).toString()
-  let prefix = ''
-
-  if (typeof env.docId === 'string') prefix = `-${env.docId}-`
-
-  return prefix + n
-}
-
 // Renders the sidenote reference in the text
 // The sidenote itself is rendered at the end of the document
 function render_sidenote_ref (tokens, idx, options, env, slf) {
-  const title = tokens[idx].meta.title
+  let title = tokens[idx].meta.title
+  if (typeof env.docId === 'string') title += `-${env.docId}`
   return `<sup id="sidenote${title}">${title}</sup>`
-}
-
-// Renderers for sidenote block (the collection of all sidenotes at the end of the document)
-function render_sidenote_block_open (tokens, idx, options) {
-  return '<!-- sidenote open -->\n'
-}
-
-// Not really needed, but for symmetry
-function render_sidenote_block_close (tokens) {
-  return '<!-- sidenote close -->\n'
 }
 
 // Renderers for individual sidenotes
 function render_sidenote_open (tokens, idx, options, env, slf) {
-  let id = slf.rules.sidenote_number(tokens, idx, options, env, slf)
-
-  // if (tokens[idx].meta.subId > 0) id += `:${tokens[idx].meta.subId}`
-
-  return  `<span id="sidenote${id}" class="sidenote">\n`
+  let title = tokens[idx].meta.title
+  if (typeof env.docId === 'string') title += `-${env.docId}`
+  return  `<span id="sidenote${title}" class="sidenote"><strong>${title}</strong>\n`
 }
 
 // Not really needed, but for symmetry
@@ -70,14 +47,9 @@ export default function sidenote_plugin (md) {
   const isSpace = md.utils.isSpace
 
   md.renderer.rules.sidenote_ref          = render_sidenote_ref
-  md.renderer.rules.sidenote_block_open   = render_sidenote_block_open
-  md.renderer.rules.sidenote_block_close  = render_sidenote_block_close
   md.renderer.rules.sidenote_open         = render_sidenote_open
   md.renderer.rules.sidenote_close        = render_sidenote_close
-  md.renderer.rules.sidenote_style        = render_sidenote_style
-
-  // helpers (only used in other rules, no tokens are attached to those)
-  md.renderer.rules.sidenote_number       = render_sidenote_number
+  // md.renderer.rules.sidenote_style        = render_sidenote_style
 
   // Process sidenote block definition
   // Example:
@@ -103,12 +75,14 @@ export default function sidenote_plugin (md) {
     if (silent) return true
 
     if (!state.env.sidenotes) state.env.sidenotes = []
-    const title = state.src.slice(start + 2, pos - 2)
+    const title = state.src.slice(start + 2, pos - 1)
 
-    const token_fref_o = new state.Token('sidenote_reference_open', '', 1)
-    token_fref_o.meta  = { title }
-    token_fref_o.level = state.level++
-    state.tokens.push(token_fref_o)
+    const token_sref_o = new state.Token('sidenote_open', 'span', 1)
+    token_sref_o.level = state.level++
+    token_sref_o.attrPush(['class', 'sidenote'])
+    token_sref_o.attrPush(['title', `sidenote${title}`])
+    token_sref_o.meta = { title }
+    state.tokens.push(token_sref_o)
 
     const oldBMark = state.bMarks[startLine]
     const oldTShift = state.tShift[startLine]
@@ -146,6 +120,7 @@ export default function sidenote_plugin (md) {
       state.sCount[startLine] += state.blkIndent
     }
 
+    console.log(startLine, endLine)
     state.md.block.tokenize(state, startLine, endLine, true)
 
     state.parentType = oldParentType
@@ -154,9 +129,9 @@ export default function sidenote_plugin (md) {
     state.sCount[startLine] = oldSCount
     state.bMarks[startLine] = oldBMark
 
-    const token_fref_c = new state.Token('sidenote_reference_close', '', -1)
-    token_fref_c.level = --state.level
-    state.tokens.push(token_fref_c)
+    const token_sref_c = new state.Token('sidenote_close', 'span', -1)
+    token_sref_c.level = --state.level
+    state.tokens.push(token_sref_c)
 
     return true
   }
@@ -195,9 +170,8 @@ export default function sidenote_plugin (md) {
         tokens
       )
 
-      const token = new state.Token('sidenote_ref', '', 1)
-      token.meta = { title: title }
-      state.tokens.push(token)
+      const token = state.push('sidenote_ref', '', 0)
+      token.meta = { title }
     }
 
     state.pos = labelEnd + 1
@@ -239,9 +213,8 @@ export default function sidenote_plugin (md) {
       if (!(state.env.sidenotes.includes(title))) {
         state.env.sidenotes.push(title)
       }
-      const token = new state.Token('sidenote_ref', '', 0)
-      token.meta = { title: title }
-      state.tokens.push(token)
+      const token = state.push('sidenote_ref', '', 0)
+      token.meta = { title }
     }
 
     state.pos = pos
@@ -249,76 +222,15 @@ export default function sidenote_plugin (md) {
     return true
   }
 
-  // Glue sidenote tokens to end of token stream
-  function sidenote_tail (state) {
-    let tokens
-    let current
-    let currentLabel
-    let insideRef = false
-    const refTokens = {}
-
-    if (!state.env.sidenotes) { return }
-
-    state.tokens = state.tokens.filter(function (tok) {
-      if (tok.type === 'sidenote_reference_open') {
-        insideRef = true
-        current = []
-        currentLabel = tok.meta.label
-        return false
-      }
-      if (tok.type === 'sidenote_reference_close') {
-        insideRef = false
-        // prepend ':' to avoid conflict with Object.prototype members
-        refTokens[':' + currentLabel] = current
-        return false
-      }
-      if (insideRef) { current.push(tok) }
-      return !insideRef
-    })
-
-    if (!state.env.sidenotes.list) { return }
-    const list = state.env.sidenotes.list
-
-    state.tokens.push(new state.Token('sidenote_block_open', '', 1))
-    const token_style = new state.Token('sidenote_style', '', 0)
-    state.tokens.unshift(token_style)
-
-    for (let i = 0, l = list.length; i < l; i++) {
-      const token_fo = new state.Token('sidenote_open', '', 1)
-      token_fo.meta = { id: i, label: list[i].label }
-      state.tokens.push(token_fo)
-
-      if (list[i].tokens) {
-        tokens = []
-
-        const token_i = new state.Token('inline', '', 0)
-        token_i.children = list[i].tokens
-        token_i.content = list[i].content
-        tokens.push(token_i)
-
-      } else if (list[i].label) {
-        tokens = refTokens[`:${list[i].label}`]
-      }
-
-      if (tokens) state.tokens = state.tokens.concat(tokens)
-
-      let lastParagraph
-
-      if (state.tokens[state.tokens.length - 1].type === 'paragraph_close') {
-        lastParagraph = state.tokens.pop()
-      } else {
-        lastParagraph = null
-      }
-
-      if (lastParagraph) {
-        state.tokens.push(lastParagraph)
-      }
-
-      state.tokens.push(new state.Token('sidenote_close', '', -1))
+  // Inject the CSS style for sidenotes
+  function sidenote_css (state, silent) {
+    if (!silent) {
+      const token = state.push('sidenote_style', '', 0)
+      token.meta = {}
     }
-
-    state.tokens.push(new state.Token('sidenote_block_close', '', -1))
+    return false
   }
+
    function debug (state) {
     console.log(state)
    }
@@ -326,6 +238,5 @@ export default function sidenote_plugin (md) {
   md.block.ruler.before('reference', 'sidenote_def', sidenote_def, { alt: ['paragraph', 'reference'] })
   md.inline.ruler.after('image', 'sidenote_inline', sidenote_inline)
   md.inline.ruler.after('sidenote_inline', 'sidenote_ref', sidenote_ref)
-  // md.core.ruler.after('inline', 'sidenote_tail', sidenote_tail)
   md.core.ruler.push('debug_log', debug)
 };
